1901805d5454431550c973463e4d02b947255afc
SMounach <souhaila.mounach@gmail.com>
2017-12-14 16:13:37 +0000

    skull du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactformComponent } from './contactform/contactform.component';
+import { ContactlistComponent } from './contactlist/contactlist.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactformComponent,
+    ContactlistComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
