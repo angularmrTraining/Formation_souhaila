30ee670e25768a134f2f473534836a44fcb1f7f5
SMounach <souhaila.mounach@gmail.com>
2017-12-14 16:33:40 +0000

    Ajout de {formulaire de contact}

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,8 +1,78 @@
 import { Injectable } from '@angular/core';
+import { contact } from "./contact/contact";
+import { Http, Response } from "@angular/http";
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch';
+import { Observable } from "rxjs/Observable";
+import {BehaviorSubject} from  "rxjs/BehaviorSubject"
 
 @Injectable()
 export class ContactService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/contacts'; // l'url de mon api(java) en local   
+  /* 
+  // Pour le test
+  private contacts = new BehaviorSubject<any>([
+    {
+      "firstName" : "Souhaila",
+      "lastName" : "Mounach",
+      "fullName" : "Souhaila MOUNACH",
+      "email" : "Souhaila@test.com",
+    },
+    {
+      "firstName" : "Mouad",
+      "lastName" : "Mounach",
+      "fullName" : "Mouad MOUNACH",
+      "email" : "Mouad@test.com",
+    },
+    {
+      "firstName" : "Marwa",
+      "lastName" : "Mounach",
+      "fullName" : "Marwa MOUNACH",
+      "email" : "Mawa@test.com",
+    }
+  ]);
+  contact = this.contacts.asObservable();
+  */
+  
+   constructor(private http: Http) {
+   }
+  
+  
+  
+  findAll(): Observable<contact[]>  {
+     return this.http.get(this.apiUrl)
+       .map((res:Response) => res.json())
+       .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+   }
+  
+   findById(id: number): Observable<contact> {
+    return this.http.get(this.apiUrl + '/' + id)
+    .map((res:Response) => res.json())
+    .catch((error:any) => Observable.throw(error.json().error || 'Error'));
+
+   }
+  
+   saveContact(conTact: contact): Observable<contact> {
+    return this.http.post(this.apiUrl, conTact)
+    .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+   }
+  
+   deleteContactById(id: number): Observable<boolean> {
+    return this.http.delete(this.apiUrl + '/' + id)
+    .map((res:Response) => res.json())
+    .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+   }
+  
+   updateContact(conTact: contact): Observable<contact> {
+    return this.http.put(this.apiUrl, conTact)
+    .map((res:Response) => res.json())
+    .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+   } 
+  
+
 
 }
--- src/app/contactform/contactform.component.ts
+++ src/app/contactform/contactform.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {FormControl, FormGroup, Validators,ReactiveFormsModule} from "@angular/forms";
+import {ContactService} from '../contact.service';
+import {ActivatedRoute, Router} from '@angular/router'
+import {contact} from '../contact/contact'
 @Component({
   selector: 'app-contactform',
   templateUrl: './contactform.component.html',
-  styleUrls: ['./contactform.component.css']
+  styleUrls: ['./contactform.component.css'],
+  providers: [ContactService]
 })
-export class ContactformComponent implements OnInit {
+export class ContactformComponent implements OnInit, OnDestroy {
+ 
 
-  constructor() { }
+  id : number;
+  conTact: contact;
+  
+  contactForm: FormGroup;
+
+  private sub: any;
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private contactService : ContactService) { }
+
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+    this.contactForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern("[^ @]*@[^ @]*"),
+      ]),
+      persPhone: new FormControl('', Validators.required),
+      proPhone: new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required)
+    });
+
+    if (this.id) { //edit form
+      this.contactService.findById(this.id).subscribe(
+        conTact => {
+            this.id = conTact.id;
+            this.contactForm.patchValue({
+            firstName: conTact.firstName,
+            lastName: conTact.lastName,
+            fullName: conTact.fullName,
+            email: conTact.email,
+            persoPhone : conTact.persoPhone,
+            proPhone : conTact.proPhone,
+            gender : conTact.gender,
+            age : conTact.age
+
+
+          });
+         },error => {
+          console.log(error);
+         }
+      );
+ 
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  onSubmit(){
+
+    let fullName : string = this.contactForm.controls['firstName'].value +" "+this.contactForm.controls['lastName'].value;
+    let conTact: contact = new contact(null,
+    this.contactForm.controls['firstName'].value,
+    this.contactForm.controls['lastName'].value,
+    fullName,
+    this.contactForm.controls['email'].value,
+    this.contactForm.controls['persoPhone'].value,
+    this.contactForm.controls['proPhone'].value,
+    this.contactForm.controls['gender'].value,
+    this.contactForm.controls['age'].value);
+    this.contactService.saveContact(conTact).subscribe();
+    this.contactForm.reset();
+    this.router.navigate(['']);
   }
 
 }
