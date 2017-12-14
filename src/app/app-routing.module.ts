8aede6a20e1102df59468116314cad6b27b62e29
SMounach <souhaila.mounach@gmail.com>
2017-12-14 16:35:41 +0000

    Ajout de {list des contacts}

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactformComponent } from './contactform/contactform.component';
+import { AboutComponent } from './about/about.component';
+import { ContactslistComponent } from './contactslist/contactslist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: ContactslistComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact/add',
+    component: ContactformComponent
+  },
+  {
+    path: 'contact/update/:id',
+    component: ContactformComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/contactslist/contactslist.component.ts
+++ src/app/contactslist/contactslist.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import {ContactService} from '../contact.service';
+import {contact} from '../contact/contact';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-contactslist',
   templateUrl: './contactslist.component.html',
-  styleUrls: ['./contactslist.component.css']
+  styleUrls: ['./contactslist.component.css'],
+  providers:[ContactService]
 })
 export class ContactslistComponent implements OnInit {
 
-  constructor() { }
+  contacts = [];
+  constructor(private router: Router,private contactService : ContactService) { }
 
   ngOnInit() {
+    this.getAllContacts();
   }
 
+  getAllContacts() {
+    this.contactService.findAll().subscribe(
+      contacts => {
+        this.contacts = contacts;
+      },
+      err => {
+        console.log(err);
+      }
+ 
+    );
+  }
+
+  editContact(conTact: contact) {
+    if (conTact) {
+      this.router.navigate(['/contact/update', conTact.id]);
+    }
+  }
+
+   deleteContact(conTact: contact) {
+      if (conTact) {
+        this.contactService.deleteContactById(conTact.id).subscribe(
+          res => {
+            this.getAllContacts();
+            this.router.navigate(['']);
+            console.log('done');
+          }
+        );
+      }
+    }
 }
